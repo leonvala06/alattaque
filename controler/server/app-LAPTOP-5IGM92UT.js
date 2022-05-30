3af5e1052d6f2748df1e4917354afe0a803bb2c2
Léon Valabrègue <leon.valabregue@mines-ales.org>
2022-05-29 21:00:31 -0300

    Remonter du niveau board au niveau game les variables du jeu

---
+++ controler/server/app-LAPTOP-5IGM92UT.js
@@ -0,0 +1,10 @@
+const express = require('express')
+const app = express()
+const port = 4000
+const path_react_app = 'C:/Users/leloune/OneDrive - IMT MINES ALES/Documents/MINES/S8/UFRJ/ProgAv/alattaque/client_3000/build'
+
+app.use(express.static(path_react_app))
+
+app.listen(port, () => {
+      console.log(`App listening at http://localhost:${port}`)
+  })
