009e84bfd70bf83fca317aaa44d65999992f4cf6
Léon Valabrègue <leon.valabregue@mines-ales.org>
2022-05-31 16:32:49 -0300

    Interface simplified : no history

--- controler/server/app.js
+++ controler/server/app.js
@@ -1,11 +1,15 @@
 const express = require('express')
 const app = express()
 const port = 4000
+const path_react_app = 'C:/Users/leloune/OneDrive - IMT MINES ALES/Documents/MINES/S8/UFRJ/ProgAv/alattaque/client_3000/build'
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
+app.use(express.static(path_react_app))
+
+app.post('/', (req, res ) => {
+    console.log(req.body);
+    res.json("Request sended " );
 })
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
-})
+      console.log(`App listening at http://localhost:${port}`)
+  })
